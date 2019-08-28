d06b3f05dcc8884e3a76c0203a188c1f009030cb
WildcatsC <octsteven@gmail.com>
2019-08-27 20:27:53 -0700

    techxml_demo

---
+++ techx-ml-pokedex-base/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, make_response
+from functools import wraps, update_wrapper
+from PIL import Image
+
+# Initialize the predictor object.
+
+# Initialize Flask app.
+
+# Base endpoint to perform prediction.
+		
+if __name__ == '__main__':
+   app.run()
