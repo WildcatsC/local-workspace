08e2b6732320339852df43815608beb777ae9a92
WildcatsC <octsteven@gmail.com>
2019-10-12 23:34:59 -0700

    nodejs

--- node/app.js
+++ node/app.js
@@ -10,18 +10,28 @@ var eventEmitter = new events.EventEmitter();
 eventEmitter.on('my_event', () => {
     console.log('data received succesfully.');
 });
-eventEmitter.emit('my_event');
+//eventEmitter.emit('my_event');
+
+events._name = 1;
+console.log(events._name)
 
 class Person extends events {
-    constructor(name) {
-        super();
-        this._name = name;
-        console.log(name);
+    constructor(name, age) {
+        super(); // allow to use “this” 
+        this._name = name; // 这个下划线？强👍
+        this._age = age;
+        console.log(age);
     }
     get name() {
         return this._name;
     }
 }
 
-let p = new Person("P");
-//console.log(p);
+const me = new Person("Steven", 20);
+console.log(me._age);
+
+me.on('name', () => {
+    console.log("my name is " + me)
+})
+
+eventEmitter.emit('name');
