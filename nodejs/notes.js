fee4346c27892e115fe885e8b8c77af70007f995
WildcatsC <octsteven@gmail.com>
2019-11-02 22:29:17 -0700

    11-2

--- nodejs/notes.js
+++ nodejs/notes.js
@@ -9,19 +9,32 @@ console.log(y) // no good
 
 */
 
-var ob1 = { a: 1 }
-console.log(ob1.a) // var obj 就是对象
+// var ob1 = { a: 1 }
+// console.log(ob1.a) // var obj 就是对象
 
-let ob2 = { b: 1 }
-console.log(ob2.b) // same
+// let ob2 = { b: 1 }
+// console.log(ob2.b) // same
 
-//getter:
+//getter: get语法将对象属性绑定到查询该属性时将被调用的函数。
 var obj = {
-    log: ['a', 'b', 'c'], // 记得逗号
-    get latest() {
-        if (this.log.length == 0) {
-            return undefined
+        log: ['a', 'b', 'c'], // 记得逗号
+        get latest() {
+            if (this.log.length == 0) {
+                return undefined
+            }
+            return this.log[this.log.length - 1]
         }
-        return this.log[this.log.length - 1]
     }
-}
+    // console.log(obj.latest);
+    // =======================================
+
+// Array.prototype.<methods> 有很多array的方法 Mozilla有教程
+
+// about JSON:
+// JSON.parse() and JSON.stringify()
+//console.log(JSON.parse({ "key": 'sb' }.key));
+var s = { "name": 1 };
+//console.log(JSON.stringify(s));
+var r = JSON.parse(s.name);
+//console.log(r);
+// output: {"name":1} \n 1
