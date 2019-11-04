06f8dc884fa72afdf661958c3039d57a397f5f75
WildcatsC <octsteven@gmail.com>
2019-11-03 21:07:22 -0800

    js-workshop

---
+++ js-workshop/app.js
@@ -0,0 +1,20 @@
+// workshop 01
+
+const express = require('express')
+const app = express()
+const port = 1024
+
+
+function reverse(str) {
+    let result = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+    return result;
+}
+
+app.get('/', (req, res) => {
+    res.send(reverse(req.query.string));
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
