30a6ab45067378ebd1a536041ea2e94b0ae6e162
WildcatsC <octsteven@gmail.com>
2019-11-07 20:44:51 -0800

    11-7

--- leetcode/15.3-sum.java
+++ leetcode/15.3-sum.java
@@ -1,5 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+// 这个肯定就要用hash来确认找unique了
 
 /*
  * @lc app=leetcode id=15 lang=java
@@ -8,32 +8,41 @@ import java.util.List;
  */
 
 // @lc code=start
+
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // 61b作业 继续暴力来一波
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
+       // 61b作业 继续暴力来一波
+       List<List<Integer>> result = new ArrayList<List<Integer>>();
+       Set<Set<Integer>> temp_re = new HashSet<Set<Integer>>();
+       int len = nums.length;
+       int[] num1 = nums;
+       int[] num2 = nums;
+       int[] num3 = nums;
 
-        int len = nums.length - 1;
-        System.out.println(len);
-        int[] num1 = nums;
-        int[] num2 = nums;
-        int[] num3 = nums;
-
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; j++) {
-                for (int k = 0; k < len; k++) {
-                    if (num1[i] + num2[j] + num3[k] == 0 && num1[i] != num2[j] && num2[j] != num3[k]
-                            && num3[k] != num1[i]) {
-                        List<Integer> temp = new ArrayList<>(3);
-                        temp.add(0, num2[i]);
-                        temp.add(1, num2[j]);
-                        temp.add(2, num3[k]);
-                        result.add(temp);
-                    }
-                }
-            }
-        }
-        return result;
+       for (int i = 0; i < len; i++) {
+           for (int j = 0; j < len; j++) {
+               for (int k = 0; k < len; k++) {
+                   if (num1[i] + num2[j] + num3[k] == 0 && num1[i] != num2[j] && num2[j] != num3[k]
+                           && num3[k] != num1[i]) {
+                       List<Integer> temp = new ArrayList<>(3);
+                       temp.add(0, num2[i]);
+                       temp.add(1, num2[j]);
+                       temp.add(2, num3[k]);
+                       Set <Integer> test = new HashSet<>(3);
+                       test.add(num1[i]);
+                       test.add(num2[j]);
+                       test.add(num3[k]);
+                       for(Set<Integer> x: temp_re){
+                           System.out.println(x.toString());
+                           if(x != test) temp_re.add(test);
+                       }
+               }
+           }
+       }
+ 
+   } 
+   return result; 
     }
 }
 
