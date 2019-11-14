cab8f153666e55e6007ced7327d7c61f3526779a
WildcatsC <octsteven@gmail.com>
2019-11-13 21:40:52 -0800

    11-13

--- leetcode/15.3-sum.java
+++ leetcode/15.3-sum.java
@@ -13,36 +13,36 @@ import java.util.*;
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
        // 61b作业 继续暴力来一波
+       Arrays.sort(nums);
+       System.out.println(Arrays.toString(nums));
        List<List<Integer>> result = new ArrayList<List<Integer>>();
-       Set<Set<Integer>> temp_re = new HashSet<Set<Integer>>();
+//        Set<Set<Integer>> temp_re = new HashSet<Set<Integer>>();
        int len = nums.length;
+
        int[] num1 = nums;
        int[] num2 = nums;
        int[] num3 = nums;
-
+//        int i,j,k;
        for (int i = 0; i < len; i++) {
-           for (int j = 0; j < len; j++) {
-               for (int k = 0; k < len; k++) {
-                   if (num1[i] + num2[j] + num3[k] == 0 && num1[i] != num2[j] && num2[j] != num3[k]
-                           && num3[k] != num1[i]) {
+           for (int j = i + 1; j < len; j++) {
+               for (int k = j + 1; k < len; k++) {
+                   if (num1[i] + num2[j] + num3[k] == 0 ) {
                        List<Integer> temp = new ArrayList<>(3);
                        temp.add(0, num2[i]);
                        temp.add(1, num2[j]);
                        temp.add(2, num3[k]);
-                       Set <Integer> test = new HashSet<>(3);
-                       test.add(num1[i]);
-                       test.add(num2[j]);
-                       test.add(num3[k]);
-                       for(Set<Integer> x: temp_re){
-                           System.out.println(x.toString());
-                           if(x != test) temp_re.add(test);
-                       }
+                       result.add(temp);
+//                        Set<Integer> test = new HashSet<>(3);
+//                        test.add(num1[i]);
+//                        test.add(num2[j]);
+//                        test.add(num3[k]);
+                   }
                }
+
            }
+
        }
- 
-   } 
-   return result; 
+       return result;
     }
 }
 
