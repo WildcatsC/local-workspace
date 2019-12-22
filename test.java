065425269d7dbaf563d77a4e7ebf6855f75d63b4
WildcatsC <octsteven@gmail.com>
2019-12-22 23:46:14 +0800

    .

--- test.java
+++ test.java
@@ -1,14 +1,30 @@
 import java.util.*;
 
- class Test{
-	public static void main(String[] args){
+class Test {
+	public static void main(String[] args) {
 		Stack<Integer> test = new Stack<Integer>();
 		test.add(1);
 		test.add(2);
 		test.add(100);
-		System.out.print(test.toString());
+		// System.out.print(test.toString());
 		test.pop();
-		System.out.print(test.toString());
-}
+		// System.out.print(test.toString());
+
+		Set<Integer> set = new HashSet<Integer>();
+		set.add(1);
+		// System.out.println(set.toString() + "haha this is a set");
+
+		List<List<Integer>> l = new ArrayList<List<Integer>>();
+		List<Integer> row = new ArrayList<Integer>();
+		row.add(1);
+		row.add(2);
+		l.add(row);
+		for(List<Integer> k : l){
+			System.out.println(l.get(0)==row);
+		}
+		
+
+
+	}
 
 }
